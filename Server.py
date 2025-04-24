730c1b211a21e31e562121db0f7efccf63f7f25c
ZanhNe <baodoanh1102004@gmail.com>
2025-04-24 18:37:59 +0700

    Add signal and server class

---
+++ Server.py
@@ -0,0 +1,98 @@
+from socket import *
+import os
+
+base_dir = os.path.dirname(__file__)
+
+class Server:
+    def __init__(self):
+        self.server = socket(AF_INET, SOCK_STREAM)
+        self.is_running = True
+    def run_server(self, host, port):
+        self.server.bind((host, port))
+        self.server.settimeout(1)
+        self.server.listen(5)
+        print(f"Server running on http://{host}:{port}")
+
+        try:
+            while self.is_running:
+                try:
+                    connection, addr = self.server.accept()
+                    request = connection.recv(1024) 
+                    response = self.resolve_request(request)
+                    if response:
+                        connection.sendall(response)
+                    connection.close()
+
+                except timeout:
+                    continue
+        except Exception as e:
+            print(e)
+        finally:
+            self.shutdown()
+            print("Server has closed")
+
+    def shutdown(self):
+        self.server.close()
+
+    def signal(self, sig, frame):
+        self.is_running = False
+    
+    def resolve_request(self, request: bytes):
+        decode_request = request.decode()
+        [method, uri, *rest] = decode_request.split(' ')
+        if method == 'GET':
+            if 'static' in uri:
+                try:
+                    with open(f"{base_dir}{uri.replace('/', '\\')}", 'r') as file:
+                        static_content = file.read()
+                    type = ""
+                    if uri.endswith('.css'):
+                        type = "text/css"
+                    elif uri.endswith('.js'):
+                        type = "text/javascript"
+                    
+                    response = (
+                        "HTTP/1.1 200 OK\r\n",
+                        f"Content-Type: {type};charset=utf-8\r\n",
+                        f"Content-Length: {len(static_content.encode())}\r\n",
+                        "Connection: close\r\n",
+                        "\r\n",
+                        f"{static_content}"
+                    )
+
+                    return ''.join(response).encode()
+                except FileNotFoundError:
+                    response = (
+                        "HTTP/1.1 404 Not Found\r\n,"
+                        "Connection: close\r\n",
+                        "\r\n"
+                    )
+                    return ''.join(response).encode()
+            elif uri == '/':
+                with open('templates/HelloWorld.html', 'r', encoding='utf-8') as file:
+                    html_content = file.read()
+                response = (
+                    "HTTP/1.1 200 OK\r\n",
+                    "Content-Type: text/html\r\n",
+                    f"Content-Length: {len(html_content.encode())}",
+                    "Connection: close\r\n",
+                    "\r\n",
+                    f"{html_content}"
+                )
+                return ''.join(response).encode()
+        else:
+            return None
+                
+
+
+            
+        
+            
+            
+
+                
+
+        
+
+    
+
--- main.py
+++ main.py
@@ -1,60 +1,11 @@
-import socket
-import os
+from Server import Server
+import signal
 
 SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 6789
 
-base_dir = os.path.dirname(__file__)
-    
+server = Server()
+signal.signal(signal.SIGINT, server.signal)
 
-def init_server(host, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind((host, port))
-    sock.listen()
-    return sock
-
-def resolve_request(request: bytes):
-    decode_request = request.decode()
-    [method, uri, *rest] = decode_request.split(' ')
-    print(method, uri)
-    if method == 'GET':
-        if uri == '/':
-            with open('templates/HelloWorld.html', 'r', encoding='utf-8') as file:
-                html_content = file.read()
-            response = f"""
-HTTP/1.1 200 OK
-Content-Type: text/html
-Content-Length: {len(html_content.encode())}
-Connection: close
-
-{html_content}
-            """
-            return response.encode()
-        if uri.endswith('.css'):
-            with open(f"{base_dir}{uri.replace('/', '\\')}", 'r') as file:
-                css_content = file.read()
-            response = f"""
-HTTP/1.1 200 OK
-Content-Type: text/css;charset=utf-8
-Content-Length: {len(css_content.encode())}
-Connection: close
-
-{css_content}
-            """
-            return response.encode()
-        return None
-
-server = init_server(SERVER_HOST, SERVER_PORT)
-
-
-while True:
-    connection, addr = server.accept()
-
-    request = connection.recv(1024)
-    print(request)
-    response = resolve_request(request)
-    if response:
-        connection.sendall(response)
-    connection.close()
-
-
+if __name__ == '__main__':
+    server.run_server(SERVER_HOST, SERVER_PORT)
